6aceb6b12e1768f27e84c504b3f403f208e6a94a
euxhen <euxhenibi@hotmail.com>
2020-09-24 16:29:05 +0200

    Updates

---
+++ precache-manifest.26b20f21bf943a5088afd7fd35ab6df2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877f1ddfe9571b62d81bfcea5c50f60a",
+    "url": "/iCode/index.html"
+  },
+  {
+    "revision": "20ef5e7aeae3914852bd",
+    "url": "/iCode/static/css/main.0cd39cb6.chunk.css"
+  },
+  {
+    "revision": "e863363306f3d806736a",
+    "url": "/iCode/static/js/2.29c5c9e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iCode/static/js/2.29c5c9e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ef5e7aeae3914852bd",
+    "url": "/iCode/static/js/main.01b379d5.chunk.js"
+  },
+  {
+    "revision": "dd382a6152e44541f90a",
+    "url": "/iCode/static/js/runtime-main.53fbd773.js"
+  },
+  {
+    "revision": "4a3c7216a7d07c179176b824d89e0358",
+    "url": "/iCode/static/media/background.4a3c7216.jpeg"
+  },
+  {
+    "revision": "daf6e1bff77a339e3877690c0250da78",
+    "url": "/iCode/static/media/headquarter.daf6e1bf.jpg"
+  },
+  {
+    "revision": "5966f3532e7defa742c455cf80c6e675",
+    "url": "/iCode/static/media/logo3.5966f353.png"
+  },
+  {
+    "revision": "eade3bb2293e926f2357971053ba35c7",
+    "url": "/iCode/static/media/signature.eade3bb2.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/iCode/precache-manifest.26b20f21bf943a5088afd7fd35ab6df2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iCode/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
