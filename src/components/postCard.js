1f33c008eda235fda69dcd00ddfc8aea35793f56
euxhen <euxhenibi@hotmail.com>
2020-09-29 14:26:34 +0200

    change

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -171,7 +171,7 @@ class PostCard extends React.Component {
     }
     var photo = process.env.PUBLIC_URL + `/user-images/avatar.jpg`;
     if (this.props.user && this.props.token !== "guest") {
-      photo = `https://s3-us-east-2.amazonaws.com/nodeimages2/${this.props.user.photo}`;
+      photo = `https://nodeimages2.s3.amazonaws.com/${this.props.user.photo}`;
     }
 
     return (
@@ -181,7 +181,7 @@ class PostCard extends React.Component {
           <div>
             <div className="user-info">
               <img
-                src={`https://s3-us-east-2.amazonaws.com/nodeimages2/${post.postedBy.photo}`}
+                src={`https://nodeimages2.s3.amazonaws.com/${post.postedBy.photo}`}
                 alt="userphoto"
               />
 
@@ -262,7 +262,7 @@ class PostCard extends React.Component {
 
                   <div className="profile-img">
                     <img
-                      src={`https://s3-us-east-2.amazonaws.com/nodeimages2/${comment.postedBy.photo}`}
+                      src={`https://nodeimages2.s3.amazonaws.com/${comment.postedBy.photo}`}
                       alt=""
                     />
                   </div>
